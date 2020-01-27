2686b82993c3f836cf536d194cbe6dd6d690c9ce
megha <17.comp.meghaa.jotangiya@gmail.com>
2020-01-27 12:35:42 +0530

    practical2

---
+++ Practical2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Practical2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name, country;
+            do
+            {
+                Console.WriteLine("who are you?");
+                name = Console.ReadLine();
+            } while (!Regex.IsMatch(name, "^[a-zA-Z]+$"));
+
+            Console.WriteLine("where are you from?");
+            country = Console.ReadLine();
+
+            Console.WriteLine("hi,"+ name + " from " + country);
+            Console.ReadLine();
+        }
+    }
+}
