231d773a17fa6f54a7c6b9ca052c206a44e00b51
megha <17.comp.meghaa.jotangiya@gmail.com>
2020-01-27 13:14:42 +0530

    practical1

--- Practical1/Program.cs
+++ Practical1/Program.cs
@@ -10,8 +10,52 @@ namespace Practical1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Pattern 1");
+            pattern1();  //call pattern1
+
+            Console.WriteLine("Pattern 2");
+            pattern2();  //call pattern1
+
+            Console.WriteLine("Pattern 3");
+            pattern3();  //call pattern1
+
             Console.Read();
+        }
+        //pettern1
+        static void pattern1()
+        {
+            for(int i=0;i<5;i++)
+            {
+                for(int j=0;j<5;j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+        }
+        //pettern2
+        static void pattern2()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = i; j < 5; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+        }
+        //pettern3
+        static void pattern3()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
         }
     }
 }
